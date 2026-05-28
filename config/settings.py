25df3d917fe89dee2ea3db53e95dac56d79cd303
Spyro <vhnagarajrakesh@gmail.com>
2026-05-29 00:45:59 +0530

    added db config

--- config/settings.py
+++ config/settings.py
@@ -85,12 +85,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-  'default': dj_database_url.config(
-      default='postgresql://nagaraj:postgres@localhost:5432/ecommerce_db',
-      conn_max_age=600
-  )
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
